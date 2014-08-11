92a95c664a42b4e07658fed84a7d69640fd67bb1
Derek Nasfell <dnasfell@student.neumont.edu>
2014-08-11 15:06:44 -0600

    Game Class

---
+++ Tetris/Controllers/Game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris.Controllers
+{
+    public class Game
+    {
+        public GameMode Mode { get; set; }
+        public bool isRunning { get; set; }
+        public bool LeftKey { get; set; }
+        public bool RightKey { get; set; }
+        public bool DownKey { get; set; }
+        public bool LeftRotateKey { get; set; }
+        public bool RightRotateKey { get; set; }
+
+        
+        public void Play()
+        {
+
+        }
+
+        public bool isCollision()
+        {
+            return false;
+        }
+    
+        //Tetrimino moves down one per tick
+        //semiphore here - threads lock the resource until they are finished with them
+        public void Drop()
+        {
+            
+        }
+
+        //Add a row of Tetrimino's with an empty space in the middle when a "Tetris" has occured
+        public void AddRowSansOne()
+        {
+
+        }
+    }
+}
