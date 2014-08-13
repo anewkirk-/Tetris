069945dde15ab912006f4b8671f84d35a98b9bf8
anewkirk- <anewkirk@student.neumont.edu>
2014-08-13 08:35:38 -0700

    Score, GameMode, ScoreManager

---
+++ Tetris/Controllers/ScoreManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tetris.Models;
+
+namespace Tetris.Controllers
+{
+    class ScoreManager
+    {
+
+        private static SQLiteConnection _conn;
+
+        public static void Submit(Score s)
+        {
+            Connect();
+
+            Disconnect();
+        }
+
+        public static List<Score> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Connect()
+        {
+            string filepath = "highscores.sqlite";
+            if (!File.Exists(filepath))
+            {
+                SQLiteConnection.CreateFile(filepath);
+                CreateSchema();
+            }
+            if (_conn == null)
+            {
+                _conn = new SQLiteConnection("Data Source="+filepath+";Version = 3;");
+                _conn.Open();
+            }
+        }
+
+        private static void Disconnect()
+        {
+            if (_conn != null)
+            {
+                _conn.Close();
+            }
+        }
+
+        private static void CreateSchema()
+        {
+            Connect();
+            SQLiteCommand createScoreTable = new SQLiteCommand("CREATE TABLE scores (username VARCHAR(10), score, INT);", _conn);
+            createScoreTable.ExecuteNonQuery();
+            Disconnect();
+        }
+    }
+}
---
+++ Tetris/Models/Score.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris.Models
+{
+    class Score
+    {
+        private string _username;
+
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+        private int _scoreValue;
+
+        public int ScoreValue
+        {
+            get { return _scoreValue; }
+            set { _scoreValue = value; }
+        }
+    }
+}
