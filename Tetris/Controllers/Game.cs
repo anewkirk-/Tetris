8b8eb9171cac34a49deadab06ef3d14b60331005
Derek Nasfell <dnasfell@student.neumont.edu>
2014-08-15 14:17:27 -0600

    tBag

--- Tetris/Controllers/Game.cs
+++ Tetris/Controllers/Game.cs
@@ -14,19 +14,39 @@ namespace Tetris.Controllers
         public GameMode Mode { get; set; }
         public TetrisBoard GameBoard { get; set; }
         private Tetrimino CurrentTetrimino { get; set; }
-        
+
+        public List<Tetrimino> tBag = new List<Tetrimino>
+        {
+            new i_Tetrimino(),
+            new j_Tetrimino(),
+            new l_Tetrimino(),
+            new o_Tetrimino(),
+            new s_Tetrimino(),
+            new t_Tetrimino(),
+            new z_Tetrimino()
+        };
+
         //This method contains all the logic that happens in one discrete unit of time during a Tetris game
         public void Tick()
         {
             throw new NotImplementedException();
         }
 
+        //Get a random Tetrimino from the Tetrimino Bag
+        public void RandomTetrimino()
+        {
+            int index = rand.Next(tBag.Count);
+            Tetrimino randT = tBag[index];
+            CurrentTetrimino = randT;
+        }
+            
+     
         //Create a row of points leaving one space
         public List<Point> RowOfBlocksMinusOne()
         {
             List<Point> Blocks = new List<Point>();
             int randomY = rand.Next(0, 10);
-
+            
             for (int i = 0; i <= 9; i++)
             {
                 if (i != randomY)
@@ -43,7 +63,8 @@ namespace Tetris.Controllers
         //Move all rows of Tetromino up one
         public void MoveRowsUp()
         {
-
+            //For every Tetromino on Gameboard
+              //(X-1, y0)
         }
 
         //Add a row of Tetrimino's with an empty space in the middle when a "Tetris" has occured on the other player's screen
