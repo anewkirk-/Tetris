dd8aa404c369762ccfdfae87aba3ae2bb6a36a74
Cody S <cosimpson@student.neumont.edu>
2014-08-18 14:38:28 -0600

    Corrected the mucked up X and Y

--- Tetris/Models/TetriminoBag/o_Tetrimino.cs
+++ Tetris/Models/TetriminoBag/o_Tetrimino.cs
@@ -3,49 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace Tetris.Models.TetriminoBag
 {
     public class o_Tetrimino : Tetrimino
     {
+        public SolidColorBrush color = new SolidColorBrush(Color.FromRgb(255, 255, 0));
+        
         private int position = 1;
+        private int preY0 = 0;
+        private int preX0 = 0;
+        private int preY2 = 0;
+        private int preX2 = 0;
+        private int preY3 = 0;
+        private int preX3 = 0;
+        private int preY1 = 0;
+        private int preX1 = 0;
         public List<Point> Blocks = new List<Point>()
         {
-            new Point { X = 1, Y = 0 },
-            new Point { X = 1, Y = 1 },
-            new Point { X = 0, Y = 1 },
-            new Point { X = 0, Y = 0 }
+            new Point { Y = 1, X = 0 },
+            new Point { Y = 1, X = 1 },
+            new Point { Y = 0, X = 1 },
+            new Point { Y = 0, X = 0 }
         };
 
         public void RotateRight()
         {
             if (position == 1)
             {
-                Blocks[0].X--;
-                Blocks[1].Y--;
-                Blocks[2].X++;
-                Blocks[3].Y++;
+                setPre();
+                Blocks[0].Y--;
+                Blocks[1].X--;
+                Blocks[2].Y++;
+                Blocks[3].X++;
             }
             if (position == 2)
             {
-                Blocks[0].Y++;
-                Blocks[1].X--;
-                Blocks[2].Y--;
-                Blocks[3].X++;
+                setPre();
+                Blocks[0].X++;
+                Blocks[1].Y--;
+                Blocks[2].X--;
+                Blocks[3].Y++;
             }
             if (position == 3)
             {
-                Blocks[0].X++;
-                Blocks[1].Y++;
-                Blocks[2].X--;
-                Blocks[3].Y--;
+                setPre();
+                Blocks[0].Y++;
+                Blocks[1].X++;
+                Blocks[2].Y--;
+                Blocks[3].X--;
             }
             if (position == 4)
             {
-                Blocks[0].Y--;
-                Blocks[1].X++;
-                Blocks[2].Y++;
-                Blocks[3].X--;
+                setPre();
+                Blocks[0].X--;
+                Blocks[1].Y++;
+                Blocks[2].X++;
+                Blocks[3].Y--;
             }
             if (position == 4)
             {
@@ -57,11 +72,35 @@ namespace Tetris.Models.TetriminoBag
             }
         }
 
+        public void RotateBack()
+        {
+            Blocks[0].Y = preY0;
+            Blocks[0].X = preX0;
+            Blocks[2].Y = preY2;
+            Blocks[2].X = preX2;
+            Blocks[3].Y = preY3;
+            Blocks[3].X = preX3;
+            Blocks[1].Y = preY1;
+            Blocks[1].X = preX1;
+        }
+
+        public void setPre()
+        {
+            preY0 = Blocks[0].Y;
+            preX0 = Blocks[0].X;
+            preY2 = Blocks[2].Y;
+            preX2 = Blocks[2].X;
+            preY3 = Blocks[3].Y;
+            preX3 = Blocks[3].X;
+            preY1 = Blocks[1].Y;
+            preX1 = Blocks[1].X;
+        }
+
         public void Fall()
         {
             for (int i = 0; i < 4; i++)
             {
-                Blocks[i].X++;
+                Blocks[i].Y++;
             }
         }
     }
